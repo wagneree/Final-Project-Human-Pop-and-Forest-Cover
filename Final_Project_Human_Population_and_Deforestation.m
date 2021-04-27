822cc26dbf78014fc2c1420e5930b553c112a2fc
pdalderete <alderetp@bc.edu>
2021-04-27 18:29:23 -0400

    4/27 Input Data

--- Final_Project_Human_Population_and_Deforestation.m
+++ Final_Project_Human_Population_and_Deforestation.m
@@ -2,13 +2,29 @@
 filename = 'forest-area-as-share-of-land-area.csv';
 ForestTable = readtable(filename);
 Countries = ForestTable.Entity;
-Brazil = Countries({'Brazil'},:)
+
+idx = find(strcmp('Brazil', Countries));
+Forest_BRA = table2array(ForestTable(idx, 3:4));
+
+idx = find(strcmp('United States', Countries));
+Forest_US = table2array(ForestTable(idx, 3:4));
+
+idx = find(strcmp('Liberia', Countries));
+Forest_LIB = table2array(ForestTable(idx, 3:4));
 
 %%
 filename = 'Pop_Data.csv';
 PopTable = readtable(filename);
-PopData = table2array(PopTable)
+years = 1990:1:2020
+
+Pop_BRA = table2array(PopTable(1,5:35))';
+Pop_US = table2array(PopTable(3,5:35))';
+Pop_LIB = table2array(PopTable(2,5:35))';
 
 %%
 filename = 'GDPpercapita.csv';
 GDPTable = readtable(filename);
+
+GDP_BRA = table2array(GDPTable(1,5:34))';
+GDP_US = table2array(GDPTable(3,5:34))';
+GDP_LIB = table2array(GDPTable(2,5:34))';
